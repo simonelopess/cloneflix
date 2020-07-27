27c244d1dbfae0c05508d40d6b71156462d2f813
Simone Lopes da Silva <simlsilva@firjan.com.br>
2020-07-27 15:31:16 -0300

    Configuração de Setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Menu from './components/Menu/';
+
+
+function App() {
+  return (
+    <div>
+      <Menu />
+      
+      teste
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Menu/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Logo from '../../assets/img/Logo.png';
+import './menu.css'
+import Button from '../Button/'
+function Menu(){
+    return (
+        <nav className="Menu">
+            <a href="/">
+                <img className="Logo" src={Logo} alt="CloneFlix logo"></img>
+            </a>
+            <Button as="a" className="ButtonLink" href="/">
+                Novo vídeo
+            </Button>
+        </nav>
+    );
+}
+
+export default Menu;
