14983f832291aaf3209108153275d8f1dca621ef
Simone Lopes da Silva <simlsilva@firjan.com.br>
2020-07-28 12:01:23 -0300

    Configuração das rotas e configuraçõa das pages como SPA

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Logo from '../../assets/img/Logo.png';
+import { Link } from 'react-router-dom' ;
 import './menu.css'
 import Button from '../Button/'
 function Menu(){
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={Logo} alt="CloneFlix logo"></img>
-            </a>
-            <Button as="a" className="ButtonLink" href="/">
+            </Link>
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Novo vídeo
             </Button>
         </nav>
